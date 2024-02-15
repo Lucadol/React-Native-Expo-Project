23040914d8dd5954e3fdf472239a93ea7b50c474
Lucas Leib <lucas.leib@gmail.com>
2024-02-15 09:54:04 +0100

    adding shopping cart features

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -3,9 +3,10 @@ import { View, Text, Platform, FlatList } from 'react-native'
 
 import { useCart } from '@/src/providers/CartProvider'
 import CartListeItem from '@/src/components/CartListItem'
+import Button from '../components/Button'
 
 const CartScreen = () => {
-    const { items } = useCart()
+    const { items, total } = useCart()
 
   return (
     <View>
@@ -14,6 +15,9 @@ const CartScreen = () => {
         renderItem={({ item }) => <CartListeItem cartItem={item} />}
         contentContainerStyle={{ padding: 10 }}
       />
+
+      <Text style={{ marginTop: 20, fontSize: 20, fontWeight: '500'}}>Total: ${total}</Text>
+      <Button text="Checkout" onPress={() => {}} />
 
         {/* Use a light status bar on iOS to account for the black space above the modal */}
         <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
